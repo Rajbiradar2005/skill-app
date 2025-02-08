90612c31bbebae58b88f33a1589e3dfabc7c92c9
Krish Kothari <kothari.krish05@gmail.com>
2025-02-08 14:15:05 -0500

    fixing homepage

--- my-app/src/Homepage.js
+++ my-app/src/Homepage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
-import { auth } from "../firebase"; // Assuming firebase.js is in the parent folder
+import { auth } from "./firebase"; // Assuming firebase.js is in the parent folder
 import "./Homepage.css";
 
 const Homepage = () => {
