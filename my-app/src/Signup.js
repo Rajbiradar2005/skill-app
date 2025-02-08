9f5bab834a9975173773dd4ba0f329f49d491aca
Raj Biradar <Raj.dbira@gmail.com>
2025-02-08 18:56:49 -0500

    name_commit

--- my-app/src/Signup.js
+++ my-app/src/Signup.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from './firebase'; // Ensure the path is correct
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { useNavigate } from 'react-router-dom';
 import './Signup.css';
 
 const Signup = () => {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const navigate = useNavigate(); // Initialize navigate function
+  const navigate = useNavigate();
 
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      await updateProfile(userCredential.user, { displayName: name });
       alert('Signup successful!');
-      navigate('/Profile'); // Redirect to home after successful signup
+      navigate('/Profile');
     } catch (error) {
       setError(error.message);
     }
@@ -26,6 +28,13 @@ const Signup = () => {
       <h2>Signup</h2>
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <form onSubmit={handleSignup}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
         <input
           type="email"
           placeholder="Email"
