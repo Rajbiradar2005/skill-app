9d193e71bdbfbf351a6f06d44e256a9246991a3a
Raj Biradar <Raj.dbira@gmail.com>
2025-02-07 23:24:16 -0500

    creating database

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // Import Firestore
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC3QdQ-adptppjL4F_3z11Oq3D3guHlPqU",
   authDomain: "skill-swap-362b3.firebaseapp.com",
   projectId: "skill-swap-362b3",
-  storageBucket: "skill-swap-362b3.firebasestorage.app",
+  storageBucket: "skill-swap-362b3.appspot.com",
   messagingSenderId: "791003642718",
+ 
   appId: "1:791003642718:web:592f750b66150fd4e86dcf",
   measurementId: "G-XZVRR0EV22"
 };
@@ -19,3 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app); // Initialize Firestore
+
+export {auth, db }; // Export Firestore instance
